ad15a86942ac5698b82b5d2cfd29fa1d256e6f33
Sven K <sven@skom.de>
2020-08-08 18:26:46 +0530

    Update Deployer

--- deploy.php
+++ deploy.php
@@ -8,6 +8,16 @@ set('repository', 'https://github.com/Starraider/devTypo3ddev.git');
 set('bin/php', '/usr/local/bin/php');
 set('web_path', 'public/');
 
+host('live')
+    ->hostname('p203341.typo3server.info')
+    ->user('p203341')
+    ->configFile('~/.ssh/config')
+    ->identityFile('~/.ssh/id_rsa')
+    ->port(22)
+    ->set('branch', 'master')
+    ->set('writable_mode', 'chmod')
+    ->set('public_urls', ['https://www.skom-support.de'])
+    ->set('deploy_path', '~/html/devTypo3ddev/live');
 
 host('beta')
     ->hostname('p203341.typo3server.info')
@@ -19,6 +29,12 @@ host('beta')
     ->set('writable_mode', 'chmod')
     ->set('public_urls', ['https://beta.skom-support.de'])
     ->set('deploy_path', '~/html/devTypo3ddev/beta');
+
+host('local')
+    ->hostname('local')
+    ->set('deploy_path', getcwd())
+    ->set('vhost_nocurrent', true)
+    ->set('public_urls', ['https://ddev-typo3.ddev.site']);
 
 //********************************* */
 // Project name
