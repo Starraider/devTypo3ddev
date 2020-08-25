758497bc1212619d8db28536c859f605c318df8e
Sven K <sven@skom.de>
2020-08-25 13:04:45 +0530

    test: Fix commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,14 +1,13 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "header-max-length": [0, "always", 72],
     "type-enum": [
       "[FEATURE]",
       "[BUGFIX]",
       "[DOCS]",
       "[TASK]",
       "[!!!]",
-      "[SECURITY]",
+      "[SECURITY]"
     ],
     "type-case": "upper-case",
     "type-empty": "never",
