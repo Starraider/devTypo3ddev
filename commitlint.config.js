ceb8735ea62f050d7b99004c195d8ac064779b5d
Sven K <sven@skom.de>
2020-08-29 19:16:44 +0530

    [FEATURE](linting): Add commitlint/config-conventional for commit massge linting

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,20 @@
 module.exports = {
-  extends: '@commitlint/config-conventional',
+  extends: "@commitlint/config-conventional",
   rules: {
-    'type-enum': [
+    "type-enum": [
       2,
-      'always',
-      ['[FEATURE]', '[BUGFIX]', '[DOCS]', '[TASK]', '[SECURITY]'],
+      "always",
+      ["[FEATURE]", "[BUGFIX]", "[DOCS]", "[TASK]", "[SECURITY]", "[!!!]"],
     ],
-    'type-case': 'upper-case',
-    'type-empty': 'never',
+    "type-case": [2, "always", "upper-case"],
+    "type-empty": [2, "never"],
+    "scope-empty": [2, "always"],
+    "header-max-length": [2, "always", 52],
+    "subject-case": [2, "always", "sentence-case"],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "body-leading-blank": [1, "always"],
+    "body-max-line-length": [2, "always", 72],
+    "footer-leading-blank": [0, "always"]
   },
 };
