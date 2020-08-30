9d7ecc33fb6cb06e154894a8d942032161ce64aa
Sven K <sven@skom.de>
2020-08-30 18:15:55 +0530

    [FEATURE]: Add commitlint and husky

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,20 +1,25 @@
 module.exports = {
-  extends: "@commitlint/config-conventional",
+  extends: ["@commitlint/config-conventional", "@commitlint/parse"],
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(\[\w*\])?:\(?(\w*)\)?\s(.+)/,
+      headerCorrespondence: ["type", "scope", "subject"],
+    },
+  },
   rules: {
     "type-enum": [
       2,
       "always",
-      ["[FEATURE]", "[BUGFIX]", "[DOCS]", "[TASK]", "[SECURITY]", "[!!!]"],
+      ["[FEATURE]", "[BUGFIX]", "[DOCS]", "[TASK]", "[SECURITY]"],
     ],
     "type-case": [2, "always", "upper-case"],
     "type-empty": [2, "never"],
-    "scope-empty": [2, "always"],
-    "header-max-length": [2, "always", 52],
+    "header-max-length": [2, "always", 80],
     "subject-case": [2, "always", "sentence-case"],
     "subject-empty": [2, "never"],
     "subject-full-stop": [2, "never", "."],
-    "body-leading-blank": [1, "always"],
+    "body-leading-blank": [2, "always"],
     "body-max-line-length": [2, "always", 72],
-    "footer-leading-blank": [0, "always"]
+    "footer-leading-blank": [2, "always"],
   },
 };
